dda2deb2aafe499d0f8ab2d48e62417991bbb1b5
ggamcandy <114662013+ggamcandy@users.noreply.github.com>
2022-12-27 16:09:15 +0700

    Add files via upload

---
+++ tast.js
@@ -0,0 +1,227 @@
+var product = [{
+    id: 1,
+    img: 'Plan B รักพี่...ไม่มีหลอก.jpg',
+    name: 'Plan B รักพี่...ไม่มีหลอก',
+    price: 490 ,
+    description: 'ใครจะไปคิด ว่าคนที่คุยกับเขาในแอป จะเป็นเด็กมอปลาย!',
+    type: 'Drama'
+}, {
+    id: 2,
+    img: 'ฆ่าหมาป่า.jpg',
+    name: 'ฆ่าหมาป่า',
+    price: 550,
+    description: 'ฉางเกิง เด็กหนุ่มที่อาศัยในเมืองเล็ก ๆ แสนสงบตรงชายแดน เคยมีเหตุโดนฝูงหมาไล่ล่าจนเกือบเอาชีวิตไม่รอด โชคดีที่สองพี่น้องตระกูลเสิ่นผ่านมาช่วยไว้ได้ทัน ฉางเกิงจึงกราบ เสิ่นสือลิ่ว ผู้น้องเป็นพ่อบุญธรรมเพื่อทดแทนบุญคุณช่วยชีวิต ทว่าชายผู้นี้นอกจากหน้าตาที่งดงามก็ไม่มีอะไรพึ่งพาได้อีก ฉางเกิงต้องพยายามฝึกฝนตัวเองให้เป็นผู้ใหญ่พึ่งพาได้เร็ว ๆ เพื่อดูแลครอบครัวและพ่อบุญธรรมน้อยของเขา ทว่าวันหนึ่งความสงบสุขเล็ก ๆ เหล่านี้ก็เปลี่ยนไป เมื่อกองทัพอนารยชนนอกด่านบุกเข้ามา เส้นทางชีวิตที่เคยเรียบง่ายของเขาไม่เหมือนเดิมอีกแล้ว แม้แต่มารดา หรือพ่อบุญธรรมน้อยของเขาก็มีสถานะเปลี่ยนไป !',
+    type: 'Romantic'
+}, {
+    id: 3,
+    img: 'ชาลาในอนธการ เล่ม 1.jpg',
+    name: 'ชาลาในอนธการ เล่ม 1',
+    price: 450,
+    description: 'เจอกันทีเป็นต้องฟาดฟันด้วยฝีปากทุกครั้งไปท่ามกลางปริศนาและเสียงร่ำไห้ของญาติผู้เสียหายลั่วเหวินโจวกับเฟ่ยตู้จำต้องร่วมมือกันเพื่อตามหาตัวคนร้ายและไขคดีให้กระจ่าง',
+    type: 'Drama'
+}, {
+    id: 4,
+    img: 'ซูซงหยวน.jpg',
+    name: 'ซูซงหยวน',
+    price: 490,
+    description: 'ซูซงหยอน เด็กน้อยผู้ที่ไม่เคยศรัทธาในชีวิต วิ่งเข้าหาความตายด้วยจิตใจที่จำยอม แต่ใครจะคิดว่าพระเจ้่าจะหยิบยื่นหัวใจให้เขาอีกครั้ง พร้อมกับข้อเเลกเปลี่ยนยิ่งใหญ่',
+    type: 'Drama'
+}, {
+    id: 5,
+    img: 'เรื่องของต้นเหมย.jpg',
+    name: 'เรื่องของต้นเหมย',
+    price: 450,
+    description: 'เขาบอกว่าจะชอบหรือไม่ชอบอะไร ของเรื่องของต้นเหมย เเต่ถ้าผมจูบเขาจะเป็นเรื่องของต้นเหมยใช่หรือเปล่า',
+    type: 'Romantic'
+}, {
+    id: 6,
+    img: 'สวรรค์ประทานพร เล่ม 1.jpg',
+    name: 'สวรรค์ประทานพร เล่ม 1',
+    price: 490,
+    description: '"เซี่ยเหลียน" สูงส่งเป็นถึงไท่จื่อ ผู้อุทิศตนช่วยเหลือปวงประชาด้วยวัยเพียงสิบเจ็ดปีก็บรรลุขึ้นสวรรค์ เป็นที่จดจำไปทั่วหล้าทำการสิ่งใดล้วนเก่งกาจปราดเปรื่อง ผู้คนต่างกล่าวว่าเขาเป็นลูกรักของสวรรค์กระทั่งเกิดเหตุพลิกผัน ทำให้เขาถูกขับออกจากสวรรค์ถึงสองครั้ง เป็นเหตุให้พลังเวทถูกผนึก ต้องลงมาอยู่ในโลกมนุษย์ ชีวิตตกต่ำกลายเป็นที่เย้ยหยันไปทั่วทั้งสามภพ ไร้สาวก ไร้ธูปเทียน ไร้คนเซ่นไหว้บูชา ถูกผู้คนลืมเลือน ต้องหาเลี้ยงชีพด้วยการเก็บขยะ ถูกจดจำว่าเป็นเทพขยะและเทพโรคระบาด ใครต่างก็คิดว่าเขาจะต้องตกต่ำไม่อาจฟื้นคืนสู่สวรรค์ ต้องเข้าสู่ทางสายมารเป็นแน่ แปดร้อยปีต่อมา เขาก็กลับขึ้นสวรรค์อีกเป็นครั้งที่สาม สร้างความสะเทือนเลื่อนลั่นไปทั่ว ทว่าขึ้นสวรรค์ครานี้เขาได้แต่รำพึงรำพัน... เฮ้อ ใครบอกว่าเป็นเทพสบาย เก็บขยะยังง่ายกว่าเป็นเทพนัก เพื่อชดใช้หนี้สวรรค์ เขาจึงรับภารกิจลงไปยังโลกมนุษย์ ทำให้เขาได้มีโอกาสพบกับ "ฮวาเฉิง" หรือพิรุณโลหิตเชยบุปผา ราชาผีผู้ออกมาจากบรรพตถงหลู สาวกคนสุดท้ายและเพียงคนเดียวของเขาตลอดกาล  "หากฝันของท่านคือช่วยเหลือปวงประชา เช่นนั้นฝันของข้า ก็คือท่านเพียงผู้เดียว" ',
+    type: 'Romantic'
+}];
+
+$(document).ready(() => {
+    var html = '';
+    for (let i = 0; i < product.length; i++) {
+        html += `<div  onclick="openProductDetail(${i})" class="product-items ${product[i].type}">
+                <img class="product-img" src="${product[i].img}" alt="">
+                <p style="font-size: 1.2vw;">${product[i].name}</p>   
+                <p stlye="font-size: 1vw;">${numberWithCommas (product[i].price)}THB</p>
+                </div>`;
+    }
+
+    $("#productlist").html(html);
+
+})
+
+function numberWithCommas(x) {
+    x = x.toString();
+    var pattern = /(-?\d+)(\d{3})/;
+    while (pattern.test(x))
+        x = x.replace(pattern, "$1,$2");
+    return x;
+}
+
+function searchsometh(elem) {
+    // console.log(elem.id)
+ var value = $('#'+elem.id).val()
+    console.log(value)
+
+    var html = '';
+    for (let i = 0; i < product.length; i++) {
+      if(product[i].name.includes (value) ) {        
+        html += `<div onclick="openProductDetail(${i})" class="product-items ${product[i].type}">
+                <img class="product-img" src="${product[i].img}" alt="">
+                <p style="font-size: 1.2vw;">${product[i].name}</p>   
+                <p stlye="font-size: 1vw;">${numberWithCommas (product[i].price)}THB</p>
+                </div>`;
+
+      }
+
+    }
+    if (html == '') {
+        $("#productlist").html(`<p> Not found Novla</p>`);
+    } else {
+        $("#productlist").html(html);
+    }
+}
+
+function searchproduct(param) {
+    console.log(param)
+    $(".product-items").css('display','none')
+    if(param == 'All') {
+        $(".product-items").css('display','block')
+    }
+    else {
+        $("."+param).css('display','block')
+    }
+}
+
+
+var productindex = 0;
+function openProductDetail(index) {
+    productindex = index;
+    console.log(productindex)
+    $("#modalDesc").css('display','flex')
+    $("#mdd-img").attr('src', product [index].img);
+    $("#mdd-name").text(product [index].name)
+    $("#mdd-price").text(numberWithCommas(product [index].price)+'THB')
+    $("#mdd-desc").text(product [index]. description)
+
+}
+
+function Closemodal() {
+    $(".modal").css('display','none')
+}
+
+var cart = [];
+function addtocart() {
+    var pass = true;
+    for (let i =0; i < cart.length; i++) {
+        if( productindex == cart[i].index) {
+            console.log('found same product')
+            cart[i].count++;
+            pass = false;
+        }
+    }
+
+    if (pass){
+        var obj ={
+            index: productindex,
+            id: product[productindex].id,
+            name: product[productindex].name,
+            price: product[productindex].price,
+            img: product[productindex].img,
+            count: 1
+        };
+        // console.log(obj)
+        cart.push(obj)
+    }
+    console.log(cart)
+
+    // Swal.fire({
+    //     icon: 'success',
+    //     title: 'Add ' + product[productindex].name + ' to cart !'
+    // })
+
+    $("#cartcount").css('display','flex').text(cart.length)
+}
+
+function openCaet(){
+    $ ('#modalCart').css('display','flex')
+    rendercart();
+}
+    
+function rendercart () {
+    if(cart.length > 0) {
+        var html = '';
+        for (let i = 0; i < cart.length; i++) {
+            html += ` <div class="cartlist-items">
+                        <div class="cartlist-left">
+                        <img src="${cart[i].img}" alt="">
+                        <div class="cartlist-detlail"> 
+                        <p style="font-size: 1.3vw;">${cart[i].name}</p>
+                        <p style="font-size: 1.2vw;">${numberWithCommas (cart[i].price * cart[i].count)} THB </p>                                                               
+                        </div>
+                    </div>
+
+                    <div class="cartlist-right">
+                        <p onclick="deinitems('-', ${i})" class="btnc">-</p>
+                        <p id="countitems${i}" style="margin: 0 20px;">${cart[i].count}</p>
+                        <p onclick="deinitems('+', ${i})" class="btnc">+</p>
+                    </div>              
+                    </div>`;
+        }
+        $("#mycart").html(html)
+    }
+    else {
+        $("#mycart").html(`<p>Not found product list</p>`)
+    }
+
+}
+
+function deinitems(action, index) {
+    if(action == '-') {
+        if(cart[index].count > 0) {
+            cart[index].count--;
+            $("#countitems"+index).text(cart[index].count)
+
+            if(cart[index].count <= 0) {
+                // Swal.fire({
+                //     icon: 'warning',
+                //     title: 'Are you sure to delete?',
+                //     showConfirmButton: true,
+                //     showCancelButton: true,
+                //     confirmButtonText: 'Delete',
+                //     cancelButtonText: 'Cancel'
+                // }).then((res) => {
+                //   if(res.isConfirmed) {
+                //      cart.splice(index, 1) 
+                //      console.log(cart)
+                //      rendercart();
+                //      $("#cartcount").css('display','flex').text(cart.length)
+                     
+                //      if(cart.length <= 0) {
+                //         $("#cartcount").css('display','none')
+                //      }
+                //   }  
+                //   else {
+                //     cart[index].count++;
+                //     $("#countitems"+index).text(cart[index].count)
+                //     rendercart();
+                //   }
+                // })
+            }
+            rendercart();
+        }
+        
+    }
+    else if(action == '+') {
+        cart[index].count++;
+        $("#countitems"+index).text(cart[index].count)
+        rendercart();
+    }
+}
+
